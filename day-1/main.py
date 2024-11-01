4e12f0f3d1c867e049777e2fbaef5cc53c64dc5e
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2024-11-01 15:01:43 +0330

    initial commit

---
+++ day-1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator.")
+city = input("What is the name of city you grow up in ?\n")
+pet = input("What is your pet name ?\n")
+print(f"Your Band Name could be {city} {pet}")
