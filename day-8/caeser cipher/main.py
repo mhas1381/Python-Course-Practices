3e2d3452f1fc61f632dfef49ec406e0d111a387b
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2024-11-09 10:08:53 +0330

    day-8 complete

--- day-8/caeser cipher/main.py
+++ day-8/caeser cipher/main.py
@@ -1,15 +1,28 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
+app_run = True
 
-direction = input("'decode' or 'encode':\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-def encrypt(text , shift):
-    encrypted_message = []
-    for position in range(len(text)):
-        new_char = text[position]
-        encrypted_message.append(new_char)
-    print(encrypted_message)
+def caesar(plain_text, shift_amount, cipher_direction):
+    cipher = []
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in plain_text:
+        if letter not in alphabet:
+            cipher.append(letter)
+        else:
+            position = alphabet.index(letter)
+            new_position = (position + shift_amount) % 26
+            cipher.append(alphabet[new_position])
+    print(f"The {cipher_direction}d code is : {"".join(cipher)}")
 
-encrypt(text = text , shift = shift)
+
+while app_run:
+    direction = input("'encode' or 'decode':\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(plain_text=text, shift_amount=shift, cipher_direction=direction)
+    user_choice = input("Do you want to use again? Y/N\n").lower()
+    if user_choice == "n":
+        print("Good bye")
+        app_run = False
