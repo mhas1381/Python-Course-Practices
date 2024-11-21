d412f63dc1db4cb9466898a0ead3939f76797f3a
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2024-11-21 21:03:36 +0330

    day-22 completed

--- day-22/ball.py
+++ day-22/ball.py
@@ -8,11 +8,26 @@ class Ball(Turtle):
         self.color("white")
         self.shapesize()
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x , new_y)
 
-    def bounce(self):
-        self.goto(0,350)
+    def y_bounce(self):
+        self.y_move *= -1
+
+    def x_bounce(self):
+        self.x_move *= -1
+        self.move_speed /= 2
+
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.x_bounce()
+
+    def reset_speed(self):
+        self.move_speed = 0.1
--- day-22/main.py
+++ day-22/main.py
@@ -2,14 +2,17 @@ from turtle import Screen
 import time
 from paddle import Paddle
 from ball import Ball
+from scoreboard import ScoreBoard
+
 screen = Screen()
 screen.title("Pong")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 
-r_paddle = Paddle((350 , 0))
-l_paddle = Paddle((-350 , 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+scoreboard = ScoreBoard()
 
 ball = Ball()
 screen.listen()
@@ -20,15 +23,29 @@ screen.onkey(key="Down", fun=r_paddle.down)
 
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     game_is_on = True
     while game_is_on:
         screen.update()
-        time.sleep(0.1)
+        time.sleep(ball.move_speed)
         ball.move()
-        if ball.ycor()>280:
-            ball.bounce()
+        # collision with wall
+        if ball.ycor() > 280 or ball.ycor() < -280:
+            ball.y_bounce()
 
+        # collision with paddle
+        if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+            ball.x_bounce()
 
+        # ball goes of the bound
+        if ball.xcor() > 380:
+            ball.reset_position()
+            ball.reset_speed()
+            scoreboard.l_point()
+
+        if ball.xcor() < -380:
+            ball.reset_position()
+            ball.reset_speed()
+            scoreboard.r_point()
 
 screen.mainloop()
