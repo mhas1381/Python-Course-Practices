3f2b6b32dd5e01b09916795fe9ae43d391db4ae3
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2024-12-02 19:07:40 +0330

    Update main.py

--- day-32/happy-birthday-wisher/main.py
+++ day-32/happy-birthday-wisher/main.py
@@ -1,57 +1,30 @@
 ##################### Extra Hard Starting Project ######################
 import datetime as dt
-import pandas as pd
+import pandas
 from random import randint
 import smtplib
+# 1. Update the birthdays.csv
+data = pandas.read_csv('birthdays.csv')
+# 2. Check if today matches a birthday in the birthdays.csv
+today = dt.date.today()
 
+birthday = data[(data.month == today.month) & (data.day == today.day)]
 
-my_email = "test_email"
-password = "password"
+names = {row['name']:row['email'] for index, row in birthday.iterrows()}
 
-def send_email(to_email , subject , body):
-    try:
-        with smtplib.SMTP("smtp.google.com") as connection:
-            connection.starttls()
-            connection.login(user = my_email , password=password)
-            message = f"Subject: {subject}\n\n{body}"
-            connection.sendmail(from_addr=my_email , to_addrs=to_email, msg=message)
-            print(f"Email sent successfully to {to_email}")
-    except Exception as e:
-        print(f"Failed to send email to {to_email}. Error: {e}")
+print(names)
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+if len(names) != 0:
+    for name in names:
+        random_letter = randint(1, 3)
+        with open(f"letter_templates/letter_{random_letter}.txt") as letter:
+            content = letter.read()
+            letter_with_name = content.replace("[NAME]", name)
+        print(letter_with_name)
+        # with smtplib.SMTP("smtp.google.com") as connection:
+        #     connection.starttls()
+        #     connection.login(user = my_email , password=password)
+        #     connection.sendmail(from_addr=my_email , to_addrs=names[name] , msg=letter_with_name)
+        print(f"Email has sent to {name} with {names[name]}")
 
-
-def check_birthday_and_send_email():
-    try:
-
-        data = pd.read_csv('birthdays.csv')
-    except FileNotFoundError:
-        print("Error: birthdays.csv not found!")
-        return
-
-
-    today = dt.date.today()
-
-
-    today_birthday = data[(data['month'] == today.month) & (data['day'] == today.day)]
-    if today_birthday.empty:
-        print("No Birthdays Today!")
-        return
-
-
-    for _, row in today_birthday.iterrows():
-        name = row['name']
-        email = row['email']
-        try:
-
-            random_letter = randint(1, 3)
-            with open(f"letter_templates/letter_{random_letter}.txt") as letter:
-                content = letter.read()
-                letter_with_name = content.replace("[NAME]", name)
-
-
-            send_email(to_email=email, subject="Happy Birthday!", body=letter_with_name)
-        except FileNotFoundError:
-            print(f"Error: Letter template {random_letter} not found!")
-            continue
-if __name__ == "__main__":
-    check_birthday_and_send_email()
+# 4. Send the letter generated in step 3 to that person's email address.
