58d8a8a5cec6e264dd6eda196d49748088701971
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2024-12-09 19:35:07 +0330

    day-37 completed

--- day-37/habit tracker/main.py
+++ day-37/habit tracker/main.py
@@ -1,9 +1,9 @@
 import requests
-
+from datetime import datetime
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 USERNAME = 'mamad1381'
 TOEKN = 'jsdjhnvosheiwjfokjokjsjklnfd'
-
+GRAPH_ID = "graph1"
 # user_params = {
 #     'token' :TOEKN,
 #     'username' : USERNAME,
@@ -15,17 +15,32 @@ TOEKN = 'jsdjhnvosheiwjfokjokjsjklnfd'
 # response.raise_for_status()
 # GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
 #
-# body = {
-#     "id":"graph1",
-#     "name":"Walking",
-#     "unit":"Km",
-#     "type":"float",
-#     "color":"shibafu"
-# }
-# headers = {
-#     "X-USER-TOKEN" :TOEKN
-# }
+body = {
+    "id":GRAPH_ID,
+    "name":"Walking",
+    "unit":"Km",
+    "type":"float",
+    "color":"shibafu"
+}
+headers = {
+    "X-USER-TOKEN" :TOEKN
+}
 # response = requests.post(url = GRAPH_ENDPOINT , json = body , headers = headers )
 # print(response.text)
 
-PIXEL_ENDPOINT = f"PIXELA_ENDPOINT/{}
+PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+today = datetime.now().strftime("%Y%m%d")
+body = {
+    # "date" : today,
+    "quantity":"14",
+
+}
+# response = requests.post(url = PIXEL_ENDPOINT , json = body , headers=headers )
+# print(response.text)
+UPDATE_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{today}"
+
+# response = requests.put(url = UPDATE_PIXEL_ENDPOINT , json = body , headers=headers )
+
+response = requests.delete(url = UPDATE_PIXEL_ENDPOINT ,headers=headers )
+
+print(response.text)
