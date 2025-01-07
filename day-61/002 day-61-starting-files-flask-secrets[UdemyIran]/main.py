525c0589033a1d335b7bbc1f3b9523ed88221347
Mohammad Hossein Asnavandi <m.asnavandi@sinacomsys.local>
2025-01-07 16:30:55 +0330

    day 60 uncompleted

---
+++ day-61/002 day-61-starting-files-flask-secrets[UdemyIran]/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fedhbfdcvnbhdyhse54t643tgvdfgb434--gbhnm'
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+@app.route("/login" , methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    form.validate_on_submit()
+    if form.validate_on_submit():
+        # Handle form submission
+        return render_template('success.html')
+    # else:
+    #     return render_template('denied.html')
+    return render_template('login.html', form=form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
