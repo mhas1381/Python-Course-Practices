978e69ba406db84cdef4d51e9f2fd655e4d3ff5e
Mohammad Hossein Asnavandi <m.asnavandi@sinacomsys.local>
2025-01-11 10:38:42 +0330

    day 61 completed

--- day-61/002 day-61-starting-files-flask-secrets[UdemyIran]/main.py
+++ day-61/002 day-61-starting-files-flask-secrets[UdemyIran]/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask_wtf.csrf import CSRFProtect
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField,EmailField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email
+from flask_bootstrap import Bootstrap5
+
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -17,27 +20,30 @@ This will install the packages from requirements.txt for this project.
 
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 app.config['SECRET_KEY'] = 'fedhbfdcvnbhdyhse54t643tgvdfgb434--gbhnm'
+bootstrap = Bootstrap5(app)
+
+class MyForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+
 
 @app.route("/login" , methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    form.validate_on_submit()
+    form = MyForm()
     if form.validate_on_submit():
-        # Handle form submission
-        return render_template('success.html')
-    # else:
-    #     return render_template('denied.html')
-    return render_template('login.html', form=form)
+        if form.email.data == 'admin@admin.com'and form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html' , form=form)
 
 
 
